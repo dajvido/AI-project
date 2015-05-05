e7491121cb3a5dcdbeeb6744854163fd6da49de8
dajvido <dawid.jaskot@gmail.com>
2015-05-05 17:30:42 +0200

    Remove hardcoded Strings in Miecio object

--- src/Miecio.scala
+++ src/Miecio.scala
@@ -1,21 +1,20 @@
 import gui.AppUI
 
-import obj.Hero
+import obj.{Values, Hero}
 
 
 object Miecio {
   def main(args: Array[String]) {
     AppUI.show()
-    AppUI.lifestyleTextArea.write("TEST TAG", "STARTING IT YAY!")
-    AppUI.lifestyleTextArea.write("TEST2 TAG", "WHAT IS GOING IN HERE?!")
-    AppUI.lifestyleTextArea.write("TEST3 TAG", "I'AM ALIVE!!!!!!!!")
-    AppUI.lifestyleTextArea.write("TEST4 TAG", "HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEY WORLD!!!!!!!!")
+    AppUI.lifestyleTextArea.write(Values.TAG_TEST, Values.VAL_TEST)
+    AppUI.lifestyleTextArea.write(Values.TAG_TEST, Values.VAL_TEST)
+    AppUI.lifestyleTextArea.write(Values.TAG_TEST, Values.VAL_TEST)
 
     val miecio = new Hero()
-    miecio.equipment += ("Glass" -> 15)
-    miecio.equipment += ("Paper" -> 4)
-    miecio.equipment += ("Plastic" -> 10)
-    miecio.equipment += ("Aluminium" -> 8)
+    miecio.equipment += (Values.ITEM_GLASS -> 15)
+    miecio.equipment += (Values.ITEM_PAPER -> 4)
+    miecio.equipment += (Values.ITEM_PLASTIC -> 10)
+    miecio.equipment += (Values.ITEM_ALUMINIUM -> 8)
     AppUI.equipmentTextArea.write(miecio.equipment)
   }
 }
--- src/obj/Values.scala
+++ src/obj/Values.scala
@@ -5,6 +5,12 @@ object Values {
   val TITLE = "AI-Project"
   val TAG_INTERRUPTED = "TIME_THREAD"
   val VAL_INTERRUPTED = "INTERRUPTED"
+  val TAG_TEST = "TEST_TAG"
+  val VAL_TEST = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" +
+    " et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex" +
+    " ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat" +
+    " nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim" +
+    " id est laborum."
 
   val BUTTON_START = "Start"
   val BUTTON_STOP = "Stop"
@@ -18,6 +24,11 @@ object Values {
   val TOWARDS_SOUTH = "SOUTH"
   val TOWARDS_EAST = "EAST"
   val TOWARDS_WEST = "WEST"
+
+  val ITEM_GLASS = "Glass"
+  val ITEM_PAPER = "Paper"
+  val ITEM_PLASTIC = "Plastic"
+  val ITEM_ALUMINIUM = "Aluminium"
 
   val FILE_BLANK = "./img/blank.png"
   val FILE_HERO = "./img/bohater.png"
