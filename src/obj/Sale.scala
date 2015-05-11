cac95b509c6a16ddd650316ceaec72786d5f5fc9
Kostek <piotr.kst@gmail.com>
2015-05-11 12:53:38 +0200

    Decision and Sale
    Files containing implementation of "Decision" Decision Tree and "Sale"
    (imo) Decision Tree.

---
+++ src/obj/Sale.scala
@@ -0,0 +1,10 @@
+package obj
+	  
+class Sale(
+			val shop: Int,
+			val item: Int,
+			val eq_quantity: Int,
+			val negotiationSuccess: Boolean = true
+			val isProper: Boolan = true,
+			val isCritHour: Boolean  false,
+          ) {}
