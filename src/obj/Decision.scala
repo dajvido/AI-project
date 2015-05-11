a5cf1514d0844ae35e978f3f9122b295f10a2f7f
dajvido <dawid.jaskot@gmail.com>
2015-05-11 14:15:07 +0200

    Add missing characters

--- src/obj/Decision.scala
+++ src/obj/Decision.scala
@@ -1,20 +1,20 @@
 package obj
 
 class Decision(
-			val hours: Int,
-			val minutes: Int,
-			val x: Int,
-			val y: Int,
-			val shop1_x: Int,
-			val shop1_y: Int,
-			val shop2_x: Int,
-			val shop2_y: Int,
-			val shop3_x: Int,
-			val shop3_y: Int,
-			val nearestItem_x: Int,
-			val nearestItem_y: Int,
-			val eq_quantity: Int,
-			val estimated_efficiency: Float,
-			val unload_need: Boolean = false,
-			val isCritHour: Boolean = false,
-          ) {}
+                val hours: Int,
+                val minutes: Int,
+                val x: Int,
+                val y: Int,
+                val shop1_x: Int,
+                val shop1_y: Int,
+                val shop2_x: Int,
+                val shop2_y: Int,
+                val shop3_x: Int,
+                val shop3_y: Int,
+                val nearestItem_x: Int,
+                val nearestItem_y: Int,
+                val eq_quantity: Int,
+                val estimated_efficiency: Float,
+                val unload_need: Boolean = false,
+                val isCritHour: Boolean = false
+                ) {}
--- src/obj/Sale.scala
+++ src/obj/Sale.scala
@@ -1,10 +1,10 @@
 package obj
-	  
+
 class Sale(
-			val shop: Int,
-			val item: Int,
-			val eq_quantity: Int,
-			val negotiationSuccess: Boolean = true
-			val isProper: Boolan = true,
-			val isCritHour: Boolean  false,
-          ) {}
+            val shop: Int,
+            val item: Int,
+            val eq_quantity: Int,
+            val negotiationSuccess: Boolean = true,
+            val isProper: Boolean = true,
+            val isCritHour: Boolean = false
+            ) {}
