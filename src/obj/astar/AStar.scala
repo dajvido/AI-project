87a55102da847a7c17ad8f3ba5464922e8e17874
dajvido <dawid.jaskot@gmail.com>
2015-05-11 09:02:22 +0200

    Refactor A*

--- src/obj/astar/AStar.scala
+++ src/obj/astar/AStar.scala
@@ -36,20 +36,7 @@ class AStar(val startPosition: (Int, Int), val targetPosition: (Int, Int)) {
         successor.current.h = estimatedDistance(successor.current)
         successor.current.f = successor.current.g + successor.current.h
 
-        var addToList = true
-        openNodes.foreach(openNode => {
-          if (openNode.current.x == successor.current.x && openNode.current.y == successor.current.y)
-            if (openNode.current.f < successor.current.f)
-              addToList = false
-
-        })
-        if (addToList)
-          closedNodes.foreach(closedNode => {
-            if (closedNode.current.x == successor.current.x && closedNode.current.y == successor.current.y)
-              if (closedNode.current.f < successor.current.f)
-                addToList = false
-          })
-        if (addToList)
+        if (shouldAddToOpenList(successor))
           openNodes.append(successor)
       })
     catch {
@@ -85,123 +72,6 @@ class AStar(val startPosition: (Int, Int), val targetPosition: (Int, Int)) {
     openNodes.remove(i)
   }
 
-  def writeTheLog(logList: ListBuffer[String]): Unit = {
-    val fw = new FileWriter(Values.FILE_LOG, true)
-    fw.write(Values.START_POS +(startPosition._1, startPosition._2) + Values.TARGET_POS +(targetPosition._1, targetPosition._2) + Values.NEXT_LINE)
-    logList.foreach(line => fw.write(line + Values.NEXT_LINE))
-    fw.write(Values.WALL)
-    fw.close()
-  }
-
-  def generatePathMap(): Unit = {
-    val foundedPath = setPath()
-    val map = Array.ofDim[String](Board.SIZE, Board.SIZE)
-    for (xi <- 0 to Board.SIZE - 1; yi <- 0 to Board.SIZE - 1) {
-      map(xi)(yi) = Values.E * 3
-      closedNodes.foreach(node => {
-        if (node.current.x == xi && node.current.y == yi) {
-          map(xi)(yi) = Values.S
-          foundedPath.foreach(pos => {
-            if ((xi, yi) == pos)
-              map(xi)(yi) = Values.X
-          })
-        }
-      })
-    }
-
-    val logList = new ListBuffer[String]
-    var logRow: String = ""
-
-    logList.append(Values.METRIC)
-    println(Values.METRIC)
-    for (yi <- 0 to Board.SIZE - 1) {
-      if (yi < 10) {
-        logRow = yi + Values.E * 2
-        print(yi + Values.E * 2)
-      } else {
-        logRow = yi + Values.E
-        print(yi + Values.E)
-      }
-      for (xi <- 0 to Board.SIZE - 1) {
-        logRow += map(xi)(yi)
-        print(map(xi)(yi))
-      }
-      logList.append(logRow)
-      println()
-    }
-    logList.append(Values.METRIC)
-    println(Values.METRIC)
-
-    writeTheLog(logList)
-  }
-
-  def getPath: ListBuffer[Direction] = {
-    val movementInstruction = new ListBuffer[Direction]()
-    if (isDifferentStartAndTargetPos) {
-      val foundedPath = setPath()
-      var lastPos = foundedPath.remove(0)
-      foundedPath.foreach(pos => {
-        if (pos._1 > lastPos._1)
-          movementInstruction.append(EAST)
-        else if (pos._1 < lastPos._1)
-          movementInstruction.append(WEST)
-        else if (pos._2 > lastPos._2)
-          movementInstruction.append(SOUTH)
-        else
-          movementInstruction.append(NORTH)
-        lastPos = pos
-      })
-    }
-    movementInstruction
-  }
-
-  def setPath(): ListBuffer[(Int, Int)] = {
-    val path = new ListBuffer[(Int, Int)]
-    if (isDifferentStartAndTargetPos) {
-      val target = closedNodes.last
-      path.append(targetPosition)
-      var pos = (target.parent.x, target.parent.y)
-      while (pos != startPosition) {
-        path.append(pos)
-        closedNodes.foreach(node => {
-          if (pos._1 == node.current.x && pos._2 == node.current.y)
-            pos = (node.parent.x, node.parent.y)
-        })
-      }
-      path.append(pos)
-    }
-    path.reverse
-  }
-
-  def estimatedDistance(successor: Pos): Double = {
-    sqrt(pow(targetPosition._1 - successor.x, 2) + pow(targetPosition._2 - successor.y, 2))
-  }
-
-  def getPos(x: Int, y: Int): Pos = {
-    Board.board(x)(y)
-  }
-
-  def isNotInClosedNodes(succ: Node): Boolean = {
-    var notInList = true
-    closedNodes.foreach(node =>
-      if (succ.current.x == node.current.x && succ.current.y == node.current.y)
-        notInList = false)
-    notInList
-  }
-
-  def getNodeOrNull(checkPos: Pos, parentNode: Node): Node = {
-    if (checkPos.canEnter) {
-      val availableNode = new Node(
-        parent = parentNode.current,
-        current = checkPos
-      )
-      if (isNotInClosedNodes(availableNode)) {
-        return availableNode
-      }
-    }
-    null
-  }
-
   def getSuccessorsList(parent: Node): ListBuffer[Node] = {
     val px = parent.current.x
     val py = parent.current.y
@@ -227,5 +97,153 @@ class AStar(val startPosition: (Int, Int), val targetPosition: (Int, Int)) {
         listSuccessors.append(availableNode)
     }
     listSuccessors
+  }
+
+  def getPos(x: Int, y: Int): Pos = {
+    Board.board(x)(y)
+  }
+
+  def getNodeOrNull(checkPos: Pos, parentNode: Node): Node = {
+    if (checkPos.canEnter) {
+      val availableNode = new Node(
+        parent = parentNode.current,
+        current = checkPos
+      )
+      if (isNotInClosedNodes(availableNode)) {
+        return availableNode
+      }
+    }
+    null
+  }
+
+  def isNotInClosedNodes(succ: Node): Boolean = {
+    var notInList = true
+    closedNodes.foreach(node =>
+      if (succ.current.x == node.current.x && succ.current.y == node.current.y)
+        notInList = false)
+    notInList
+  }
+
+  def estimatedDistance(successor: Pos): Double = {
+    sqrt(pow(targetPosition._1 - successor.x, 2) + pow(targetPosition._2 - successor.y, 2))
+  }
+
+  def shouldAddToOpenList(successor: Node): Boolean = {
+    openNodes.foreach(openNode => {
+      if (openNode.current.x == successor.current.x && openNode.current.y == successor.current.y)
+        if (openNode.current.f < successor.current.f)
+          return false
+
+    })
+    closedNodes.foreach(closedNode => {
+      if (closedNode.current.x == successor.current.x && closedNode.current.y == successor.current.y)
+        if (closedNode.current.f < successor.current.f)
+          return false
+    })
+    true
+  }
+
+  def writeToLog(logList: ListBuffer[String]): Unit = {
+    val fw = new FileWriter(Values.FILE_LOG, true)
+    fw.write(Values.START_POS +(startPosition._1, startPosition._2) + Values.TARGET_POS +(targetPosition._1, targetPosition._2) + Values.NEXT_LINE)
+    logList.foreach(line => fw.write(line + Values.NEXT_LINE))
+    fw.write(Values.WALL)
+    fw.close()
+  }
+
+  def getMapArray: Array[Array[String]] = {
+    val foundedPath = setPath()
+    val map = Array.ofDim[String](Board.SIZE, Board.SIZE)
+    for (xi <- 0 to Board.SIZE - 1; yi <- 0 to Board.SIZE - 1) {
+      map(xi)(yi) = Values.E * 3
+      closedNodes.foreach(node => {
+        if (node.current.x == xi && node.current.y == yi) {
+          map(xi)(yi) = Values.S
+          foundedPath.foreach(pos => {
+            if ((xi, yi) == pos)
+              map(xi)(yi) = Values.X
+          })
+        }
+      })
+    }
+    map
+  }
+
+  def generateTheLog(map: Array[Array[String]]): ListBuffer[String] = {
+    val logList = new ListBuffer[String]
+    var logRow: String = ""
+
+    logList.append(Values.METRIC)
+    println(Values.METRIC)
+    for (yi <- 0 to Board.SIZE - 1) {
+      if (yi < 10) {
+        logRow = yi + Values.E * 2
+        print(yi + Values.E * 2)
+      } else {
+        logRow = yi + Values.E
+        print(yi + Values.E)
+      }
+      for (xi <- 0 to Board.SIZE - 1) {
+        logRow += map(xi)(yi)
+        print(map(xi)(yi))
+      }
+      logList.append(logRow)
+      println()
+    }
+    logList.append(Values.METRIC)
+    println(Values.METRIC)
+    logList
+  }
+
+  def generatePathMap(): Unit = {
+    if (isDifferentStartAndTargetPos) {
+      val map = getMapArray
+      val logList = generateTheLog(map)
+
+      writeToLog(logList)
+    }
+  }
+
+  def setPath(): ListBuffer[(Int, Int)] = {
+    val path = new ListBuffer[(Int, Int)]
+    val target = closedNodes.last
+    var pos = (target.parent.x, target.parent.y)
+
+    path.append(targetPosition)
+    while (pos != startPosition) {
+      path.append(pos)
+      closedNodes.foreach(node => {
+        if (pos._1 == node.current.x && pos._2 == node.current.y)
+          pos = (node.parent.x, node.parent.y)
+      })
+    }
+    path.append(pos)
+    path.reverse
+  }
+
+  def findDirection: ListBuffer[Direction] = {
+    val movementInstruction = new ListBuffer[Direction]()
+    val foundedPath = setPath()
+    var lastPos = foundedPath.remove(0)
+    foundedPath.foreach(pos => {
+      if (pos._1 > lastPos._1)
+        movementInstruction.append(EAST)
+      else if (pos._1 < lastPos._1)
+        movementInstruction.append(WEST)
+      else if (pos._2 > lastPos._2)
+        movementInstruction.append(SOUTH)
+      else
+        movementInstruction.append(NORTH)
+      lastPos = pos
+    })
+    movementInstruction
+  }
+
+  def getPath: ListBuffer[Direction] = {
+    var movementInstruction = new ListBuffer[Direction]()
+    if (isDifferentStartAndTargetPos) {
+      movementInstruction = findDirection
+    }
+    movementInstruction
   }
 }
