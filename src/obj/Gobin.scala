85a9b97ec04ada381bf365b1a2de5a26ae78a244
dajvido <dawid.jaskot@gmail.com>
2015-06-08 17:23:00 +0200

    ...

--- src/obj/Gobin.scala
+++ src/obj/Gobin.scala
@@ -1,48 +1,48 @@
-class bin()
-{
-int x;
-int y;
-int costs;
-string trash;
-};
-
-bin[] bins = new bin[10];
-int[] bincosts = new int[10];
-
-void gotobin(bins)
-    {
-    i=0;
-    j=0;
-    while (i<bins.length()-1)
-        {
-        while (j<bins.length)
-            {
-            if (i<>j)
-                {
-                if (bins[j].costs()==bincosts[0] and bins[j].trash() == "a lot of")
-                    {
-                    go(bins[j].x,bins[j].y);
-                    break;
-                    }
-                elsif (bins[j].costs()==bincosts[0] and bins[j].trash() == "many")
-                    {
-                    if (bins[i].costs()==bincosts[1] and bins[i].trash() == "a lot of")
-                        {
-                        if (bincosts[1]-bincosts[0]<3)
-                            {
-                            go(bins[i].x,bins[i].y);
-                            break;
-                            }
-                        else
-                            {
-                            go(bins[j].x,bins[j].y);
-                            break;
-                            }
-                        }
-                    }
-                }
-            j+=1;
-            }
-        i+=1
-        }
-    };
+//class bin()
+//{
+//int x;
+//int y;
+//int costs;
+//string trash;
+//};
+//
+//bin[] bins = new bin[10];
+//int[] bincosts = new int[10];
+//
+//void gotobin(bins)
+//    {
+//    i=0;
+//    j=0;
+//    while (i<bins.length()-1)
+//        {
+//        while (j<bins.length)
+//            {
+//            if (i<>j)
+//                {
+//                if (bins[j].costs()==bincosts[0] and bins[j].trash() == "a lot of")
+//                    {
+//                    go(bins[j].x,bins[j].y);
+//                    break;
+//                    }
+//                elsif (bins[j].costs()==bincosts[0] and bins[j].trash() == "many")
+//                    {
+//                    if (bins[i].costs()==bincosts[1] and bins[i].trash() == "a lot of")
+//                        {
+//                        if (bincosts[1]-bincosts[0]<3)
+//                            {
+//                            go(bins[i].x,bins[i].y);
+//                            break;
+//                            }
+//                        else
+//                            {
+//                            go(bins[j].x,bins[j].y);
+//                            break;
+//                            }
+//                        }
+//                    }
+//                }
+//            j+=1;
+//            }
+//        i+=1
+//        }
+//    };
